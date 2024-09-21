0a9fdc8b897d7500a85c6114f394b234bba3add8
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-09-22 00:33:20 +0500

    intial setup

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from "../components/footer"
+
+const Home = () => {
+    return (
+        <div>
+
+            <h1 className="text-3xl font-bold underline">
+                Home
+            </h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home

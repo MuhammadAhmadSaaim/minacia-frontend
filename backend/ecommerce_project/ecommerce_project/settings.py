0f5ee9dcbbda9ac196a8fe467af56e4d979b1dca
Ali Nawaz <131615374+Ali01129@users.noreply.github.com>
2025-06-24 15:53:56 +0500

    Update settings.py for deploying and making url for admin panel

--- backend/ecommerce_project/ecommerce_project/settings.py
+++ backend/ecommerce_project/ecommerce_project/settings.py
@@ -147,6 +147,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
+#new line added by Chaudhary Ali Muhammad Nawaz
+STATIC_ROOT = '/home/ubuntu/minacia/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

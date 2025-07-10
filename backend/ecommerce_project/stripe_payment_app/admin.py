4295ff367f2f74a721e77d053654912b612bddd6
MinalAnwar <reachoutminalanwar@gmail.com>
2025-07-10 17:07:27 +0500

    all fields that require changes to make it editable or not

--- backend/ecommerce_project/stripe_payment_app/admin.py
+++ backend/ecommerce_project/stripe_payment_app/admin.py
@@ -3,13 +3,13 @@ from .models import Payment, BillingInfo
 
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'method', 'amount', 'status', 'created_at']
+    readonly_fields = ['user', 'method', 'amount', 'status', 'created_at']
     list_filter = ['method', 'status', 'created_at']
     search_fields = ['user__username', 'method']
     ordering = ['-created_at']
 
 class BillingInfoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone', 'user']
+    readonly_fields = ['address','name', 'email', 'phone', 'user']
     search_fields = ['name', 'email', 'user__username']
 
 # Register with the custom admin site
